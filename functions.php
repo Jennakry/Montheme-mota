c2899094ac421c75e74247583d4136548be74c15
jenna <je.enna.a@hotmail.com>
2023-09-22 10:24:45 +0200

    modif du menu

--- functions.php
+++ functions.php
@@ -32,3 +32,19 @@ function footer_montheme()
 }
 
 add_action('wp_footer', 'footer_montheme');
+
+//Déclare le fichier style.css
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('theme-style', get_template_directory_uri() . '/style.css');
+}
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
+//Déclare le JS//
+function theme_enqueue_scripts()
+{
+    wp_enqueue_script('scripts-1', get_stylesheet_directory_uri() . '/script.js', array(), '1.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
--- header.php
+++ header.php
@@ -4,19 +4,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- FONT SPACE MONO -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+
     <title>Mota</title>
     <?php wp_head(); ?>
+
 </head>
 
 <body>
+    <header>
+        <nav id="navigation">
+            <div class="img-logo">
+                <img src="/mota/wp-content/themes/Montheme-mota/assets/images/logo.png">
 
-    <nav id="navigation">
-        <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'main-menu',
-                'menu_id' => 'primary-menu',
-            )
-        );
-        ?>
-    </nav>
+            </div>
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'main-menu',
+                    'menu_id' => 'primary-menu',
+                )
+            );
+            ?>
+
+
+
+
+
+
+
+
+        </nav>
+    </header>
