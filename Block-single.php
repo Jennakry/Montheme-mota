5f3407fa96efb464eb90530e20d254b9805bd8d9
jenna <je.enna.a@hotmail.com>
2023-10-13 11:16:01 +0200

    Block-single mis en place

---
+++ Block-single.php
@@ -0,0 +1,50 @@
+<?php
+// On place les critères de la requête dans un Array
+$cats = array_map(function ($terms) {
+    return $terms->term_id;
+}, get_the_terms(get_post(), 'categories-photos'));
+$args = array(
+    'post__not_in' => [get_the_ID()],
+    'order_by_rand' => 'rand',
+    'post_type' => 'photos',
+    'tax_query' => [
+        [
+            'taxonomy' => 'categories-photos',
+            'terms' => $cats,
+        ]
+    ]
+);
+//On crée ensuite une instance de requête WP_Query basée sur les critères placés dans la variables $args
+$query = new WP_Query($args);
+?>
+<div class="photo_aleatoire">
+    <!-- //On vérifie si le résultat de la requête contient des articles -->
+    <?php if ($query->have_posts()) : ?>
+
+        <!-- //On parcourt chacun des articles résultant de la requête -->
+        <?php $count = 0; ?>
+        <?php while ($query->have_posts()) : ?>
+            <?php $count++; ?>
+            <?php $query->the_post(); ?>
+
+
+            <?php the_content(); ?>
+            <?php if (has_post_thumbnail()) : ?>
+
+                <div class="photo_aimerezaussi">
+                    <?php the_post_thumbnail(); ?>
+
+                </div>
+                <?php if ($count == 2) {
+                    break; // sortir de la boucle si deux photos ont été traitées
+                } ?>
+            <?php endif; ?>
+
+        <?php endwhile; ?>
+</div>
+<?php else : ?>
+    <p>Désolé, aucun article ne correspond à cette requête</p>
+<?php endif;
+    wp_reset_query();
+?>
+</section>
--- single-photos.php
+++ single-photos.php
@@ -53,10 +53,48 @@ get_header();
                 <div class="myBtn">
                     <button>Contact</button>
                 </div>
+                <div class="photo_choix">
+                    <div class="photo_avant">
+                        <?php
+                        $prev_post = get_previous_post();
+                        $next_post = get_next_post();
+
+                        if (!empty($prev_post)) {
+                            $prev_image = get_the_post_thumbnail_url($prev_post->ID);
+                            previous_post_link(
+                                '<span class="left"><img src="' .
+                                    $prev_image . '" alt="' . $prev_post->post_title . '" 
+                            width="75" height="75"/> <a href="' . get_permalink($prev_post) . '" 
+                            rel="prev"><img src="' . get_stylesheet_directory_uri() . '/assets/images/fleche-gauche.png"></a></span>',
+                                '%title',
+                                false
+                            );
+                        }
+                        ?>
+                    </div>
+                    <div class="photo_apres">
+                        <?php
+                        if (!empty($next_post)) {
+                            $next_image = get_the_post_thumbnail_url($next_post->ID);
+                            next_post_link(
+                                '<span class="right"><img src="' .
+                                    $next_image . '" alt="' . $next_post->post_title . '" 
+                            width="75" height="75"/> <a href="' . get_permalink($next_post) . '" 
+                            rel="next"><img src="' . get_stylesheet_directory_uri() . '/assets/images/fleche-droite.png"></a></span>',
+                                '%title',
+                                false
+                            );
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="section3">
+                <h2>VOUS AIMEREZ AUSSI</h2>
+
+                <?php include_once "Block-single.php"; ?>
 
             </div>
-
-            <p>VOUS AIMEREZ AUSSI</p>
         </article>
 
 
